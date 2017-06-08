026744c44191af17f8c0f6b3a7875ca7865dcf03
kiyoma <congroo@qq.com>
2017-06-08 11:05:20 +0800

    修改统计字数js（老浏览器不支持ES6语法）

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -2,15 +2,15 @@
  * Created by Administrator on 2017/6/8.
  */
 function count_words(){
-    let $editor = $("#editor");
+    var $editor = $("#editor");
     return $editor.val().length;
 }
 
 $(function(){
-    let $editor = $("#editor");
-    let $word_counter = $("#word_counter");
+    var $editor = $("#editor");
+    var $word_counter = $("#word_counter");
     $editor.on("keyup",function(){
-        let $word_num = count_words();
+        var $word_num = count_words();
         if(typeof($word_num) == "number"){
             $word_counter.text($word_num);
         }
